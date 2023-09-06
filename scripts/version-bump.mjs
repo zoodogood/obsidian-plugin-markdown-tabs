6d492e2d61304715f019e8724ed8d5c9d9bf55fb
zoodogood <zoodogood@gmail.com>
2023-09-06 22:10:13 +0300

    Move to scripts/version-bump.mjs

--- scripts/version-bump.mjs
+++ scripts/version-bump.mjs
@@ -1,6 +1,8 @@
+import * as assert from "assert";
 import { readFileSync, writeFileSync } from "fs";
 
 const targetVersion = process.env.npm_package_version;
+assert.ok(targetVersion !== undefined);
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
